eb95b326114824cfe1d5d9ede72b51ec34ae8a19
Michał Mikuła <mikulamichal@proton.me>
2023-10-13 03:39:59 +0200

    Add messaging service & hero fetching

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.sass']
 })
 export class HeroesComponent {
-  heroes = HEROES
+  heroes: Hero[] = []
   selectedHero?: Hero
   onSelect = (hero: Hero):void => {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: selected hero id=${this.selectedHero.id}`)
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => this.heroes = heroes)
+  }
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes()
   }
 }
